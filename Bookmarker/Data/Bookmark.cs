dbfc1a1b355a54a80642c124a9e8d80f38a3e5d6
Rafael Alencar <ralencar@Rafaels-MacBook-Pro.local>
2020-04-03 18:39:55 -0400

    Add files

---
+++ Bookmarker/Data/Bookmark.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Bookmarker.Data
+{
+    public class Bookmark
+    {
+        public int Id { get; set; }
+
+        public Uri BookmarkUri { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Thumbnail { get; set; }
+
+        public Category Category { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime UpdateAt { get; set; }
+
+        public Bookmark()
+        {
+        }
+    }
+}
---
+++ Bookmarker/Services/BookmarkService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Bookmarker.Data;
+
+namespace Bookmarker.Services
+{
+    public class BookmarkService : IBookmarkService
+    {
+        public BookmarkService()
+        {
+        }
+
+        public Task AddAsync(Uri url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task EditAsync(int bookmarkId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Bookmark> GetAsync(string siteName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Bookmark>> GetListAsync(int indexStart, int offset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoveAsync(int bookmarkId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
